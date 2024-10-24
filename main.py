29572f6491d756caf8db9e04dd9c7711f288406f
juliconti7 <jcontigonzalez@uade.edu.ar>
2024-10-24 10:46:35 -0300

    agregamos gestionar usuario

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import json
-from admin import registrarUsuario, listarMiembros, borrarMiembro, buscarMiembro
+from admin import registrarUsuario, listarMiembros, borrarMiembro, buscarMiembro, gestionarMiembro
 from user import verPerfil, editarPerfil
 
 def obtenerDNI():
@@ -52,6 +52,7 @@ def menuAdministrador():
         print("2. Lista de miembros")
         print("3. Borrar miembro")
         print("4. Buscar miembro")
+        print("5. Gestionar miembros")
         print("0. Salir")
 
         opcion = input("Introduce una opción: ")
@@ -68,6 +69,8 @@ def menuAdministrador():
             borrarMiembro()
         elif opcion == 4:
             buscarMiembro()
+        elif opcion == 5:
+            gestionarMiembro()
         elif opcion == 0:
             print("Hasta luego")
             break
